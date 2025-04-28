bab53e5108f9b8a3bb7ee15bd505c8c422a33048
Natalia Miklós <natalia.j.miklos@gmail.com>
2025-04-28 11:52:09 +0200

    Draft update

--- src/test_eim.py
+++ src/test_eim.py
@@ -1,41 +1,53 @@
 """
 This code segments an annotated dataset into windows, classifies them, 
 and summarizes the results. The purpose is to test an EI model locally.
+
+It is recommended to first segment the dataset into episodes.
+This ensures that less data is loaded into memory at once.
 """
 import pandas as pd
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import confusion_matrix, accuracy_score, average_precision_score, f1_score, roc_curve, recall_score
 
-from typing import List, Tuple, Any
+from typing import List, Tuple, Optional
 from pathlib import Path
 
 from get_env import get_input_path, get_output_path
-from handle_csv import read_csv_to_pandas_dataframe
+from handle_csv import read_csv_to_pandas_dataframe, get_all_csv_files_in_directory
 from classify_eim import load_model, close_loaded_model, classify_window, get_top_prediction
 
-
-def init_confusion_matrix(actual_annotations: List[str], predicted_annotations: List[str], classes: List[str]):
-    confusion_matrix = confusion_matrix(actual_annotations, predicted_annotations, labels=classes)
-    return confusion_matrix
 
 def format_window_for_classification(df: pd.DataFrame) -> List[float]:
     """
     Flattens a DataFrame into a single Python list.
-
-    - .values gives a 2D NumPy array
-    - .ravel() flattens it
-    - .tolist() turns it into a Python list
 
     Args:
         df (pd.DataFrame): The input DataFrame containing the data of a single window to be classified.
 
     Returns:
         List[Any]: A flattened list of floats to be classified.
+
+    Notes:
+        - .values gives a 2D NumPy array
+        - .ravel() flattens it
+        - .tolist() turns it into a Python list
+        - the elements are casted into floats
     """
     flattened_features = df.values.ravel().tolist()
     features = [float(f) for f in flattened_features]
     return features
 
 def validate_input(total_rows: int, window_size: int, overlap_size: int) -> bool:
+    """
+    Validates input against each other.
+
+    Args:
+        total_rows (int): The number of rows in the dataset.
+        window_size (int): The number of rows in each segmented window.
+        overlap_size (int): The number of overlapping rows between consecutive windows.
+
+    Returns:
+        bool: Returns True if the input values are valid, throws a ValueError otherwise.
+    """
     if total_rows == 0:
         raise ValueError('The input DataFrame is empty. There is no data to segment.')
     
@@ -53,7 +65,7 @@ def validate_input(total_rows: int, window_size: int, overlap_size: int) -> bool
 
     return True
 
-def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: int, model_file_path: Path) -> Tuple[List[str], List[str]]:
+def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: int, model_file_path: Path) -> Optional[Tuple[List[str], List[str]]]:
     """
     Segments a DataFrame into overlapping windows and classifies each.
 
@@ -64,7 +76,7 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
         model_file_path (Path): Path to the .eim model file.
 
     Returns:
-        List[str]: A list of predicted labels for all windows. # TODO
+        Tuple[List[str], List[str]]: If input is valid, a Tuple with actual and predicted annotations for all windows.
     """
     total_rows = len(df)
 
@@ -97,23 +109,69 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
 
         return actual_annotations, predicted_annotations
 
+def init_confusion_matrix(actual_annotations: List[str], predicted_annotations: List[str], classes: List[str]):
+    confusion_matrix = confusion_matrix(actual_annotations, predicted_annotations, labels=classes)
+    return confusion_matrix
+
+def generate_report(output_dir_path: Path, actual_annotations: List[str], predicted_annotations: List[str]):
+    # time it took to  classify all
+    # peak memory during classification
+
+    confusion_matrix = init_confusion_matrix(actual_annotations, predicted_annotations)
+    accuracy = accuracy_score(actual_annotations, predicted_annotations)
+    area_under_roc_curve = roc_curve(actual_annotations, predicted_annotations)
+    weighted_avg_precision = average_precision_score(actual_annotations, predicted_annotations)
+    weighted_avg_recall = recall_score(actual_annotations, predicted_annotations)
+    weighted_avg_f1_score = f1_score(actual_annotations, predicted_annotations)
+
+def process_files(window_size: int, window_overlap: int, model_file_path: Path, classes: Tuple[str], input_dir_path: Path, output_dir_path) -> None:
+    """
+    Processes all CSV files in the input directory by segmenting them into windows,
+    classfying the windows, and generating a classficiation result in the output directory.
+
+    Args:
+        window_size (int): The number of rows in each segmented window.
+        overlap_size (int): The number of overlapping rows between consecutive windows.
+        model_file_path (Path): Path to the .eim model file.
+        classes (Tuple[str]): Classes meant to be classified by the selected model.
+        input_dir (Path): Directory containing input CSV files.
+        output_dir (Path): Directory to write output files.
+    
+    Returns:
+        None
+    """
+    files = get_all_csv_files_in_directory(input_dir_path)
+    predicted_annotations = list()
+    actual_annotations = list()
+
+    for file in files:
+        episode_df = read_csv_to_pandas_dataframe(file)
+        ep_actual_annotations, ep_predicted_annotations = classify_window_by_window(episode_df, window_size, window_overlap, model_file_path)
+        actual_annotations.append(ep_actual_annotations)
+        predicted_annotations.append(ep_predicted_annotations)
+
+    generate_report(output_dir_path, actual_annotations, predicted_annotations)
+
 
 if __name__ == '__main__':
+    # Parameters to be set
     window_size = 300
     window_overlap = 150
+    model_file_name = 'single-model-approach-linux-x86_64-v5.eim'
+
+    # Classes
+    transitions_model_classes = ('getting up', 'leaving home', 'entering home', 'preparing for bed', 'airing', 'other')
+    routines_model_classes = ('sleeping', 'working', 'working out', 'relaxing', 'other')
+    food_model_classes = ('preparing breakfast', 'eating breakfast', 'preparing dinner', 'eating dinner', 
+        'preparing supper', 'eating supper', 'preparing a drink', 'preparing a meal', 'eating a meal', 'other')
+    single_model_classes = tuple(dict.fromkeys(transitions_model_classes + routines_model_classes + food_model_classes))
+
+    classes = single_model_classes
 
     # Paths
     input_dir_path = get_input_path()
     output_dir_path = get_output_path()
     output_dir_path.mkdir(parents=True, exist_ok=True)
+    model_file_path = input_dir_path / model_file_name
 
-    model_file_path = input_dir_path / 'single-model-approach-linux-x86_64-v5.eim'
-
-    dataset_file_name = 'synchronized_merged_selected_annotated_new_col_names.csv'
-    input_dataset_path = input_dir_path / dataset_file_name
-
-    # grab all episode files in directory
-    # for every episode file, classify as many windows from every file
-    episode_df = read_csv_to_pandas_dataframe(input_dataset_path)
-
-    actual_annotations, predicted_annotations = classify_window_by_window(episode_df, window_size, window_overlap, model_file_path)
+    process_files(window_size, window_overlap, model_file_path, classes, input_dir_path, output_dir_path)
