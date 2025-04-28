196e0428ae35c44a77a4689403367595672810f8
Natalia Miklós <natalia.j.miklos@gmail.com>
2025-04-28 17:59:34 +0200

    Subdivided report function

--- src/test_eim.py
+++ src/test_eim.py
@@ -1,14 +1,17 @@
 """
-This code segments an annotated dataset into windows, classifies them, 
-and summarizes the results. The purpose is to test an EI model locally.
+This code segments an annotated dataset into windows, classifies them, and summarizes the results. 
+The purpose is to test an EI model locally.
 
-It is recommended to first segment the dataset into episodes.
-This ensures that less data is loaded into memory at once.
+It is recommended to first segment the dataset into episodes. This ensures that less data is loaded into memory at once.
+
+The data is expected to contain only those columns that were used in training (e.g., if the time column was not
+used for training the model, it should be excluded from the data used for the inference as well).
 """
 import pandas as pd
 from sklearn.metrics import confusion_matrix, accuracy_score, average_precision_score, f1_score, roc_curve, recall_score
 import time
 import tracemalloc
+import json
 
 from typing import List, Tuple, Optional
 from pathlib import Path
@@ -19,25 +22,26 @@ from handle_csv import read_csv_to_pandas_dataframe, get_all_csv_files_in_direct
 from classify_eim import load_model, close_loaded_model, classify_window, get_top_prediction
 
 
-def format_window_for_classification(df: pd.DataFrame, relevant_columns: Optional[List[str]]) -> List[float]:
+def format_window_for_classification(df: pd.DataFrame) -> List[float]:
     """
     Flattens a DataFrame into a single Python list.
 
     Args:
         df (pd.DataFrame): The input DataFrame containing the data of a single window to be classified.
-        relevant_columns (Optional[List[str]]): The list of columns expected by the model, all other are dropped.
 
     Returns:
         List[Any]: A flattened list of floats to be classified.
 
     Notes:
+        An EI model accepts data of a window as a continous list of comma separated values
+        (i.e., every rows of values ends with a comma and the next row starts in the same line).
+
+        In this code:
         - .values gives a 2D NumPy array
         - .ravel() flattens it
         - .tolist() turns it into a Python list
         - the elements are casted into floats
     """
-    if relevant_columns:
-        df = df[list(relevant_columns)]
     flattened_features = df.values.ravel().tolist()
     features = [float(f) for f in flattened_features]
     return features
@@ -71,7 +75,7 @@ def validate_input(total_rows: int, window_size: int, overlap_size: int) -> bool
 
     return True
 
-def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: int, loaded_model: ImpulseRunner, relevant_columns: Optional[List[str]]) -> Optional[Tuple[List[str], List[str]]]:
+def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: int, loaded_model: ImpulseRunner) -> Optional[Tuple[List[str], List[str]]]:
     """
     Segments a DataFrame into overlapping windows and classifies each.
 
@@ -80,7 +84,6 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
         window_size (int): The number of rows in each segmented window.
         overlap_size (int): The number of overlapping rows between consecutive windows.
         loaded_model (ImpulseRunner): Loaded .eim model.
-        relevant_columns (Optional[List[str]]): The list of columns expected by the model, all other are dropped.
 
     Returns:
         Tuple[List[str], List[str]]: If input is valid, a Tuple with actual and predicted annotations for all windows.
@@ -99,10 +102,10 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
 
             window = df.iloc[start_position : end_position]
 
-            last_annotation = window['annotation'].iloc[-1]
+            last_annotation = window['annotation'].iloc[-1] # TODO
             actual_annotations.append(last_annotation)
 
-            formatted_window = format_window_for_classification(window, relevant_columns)
+            formatted_window = format_window_for_classification(window)
             classification_result = classify_window(loaded_model, formatted_window)
             prediction_class, _ = get_top_prediction(classification_result)
             predicted_annotations.append(prediction_class)
@@ -112,18 +115,35 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
 
         return actual_annotations, predicted_annotations
 
-def generate_report(output_dir_path: Path, actual_annotations: List[str], predicted_annotations: List[str], 
-                    classes: Tuple[str], total_time_seconds: float, peak_memory_mb: float):
+def save_report_to_json_file(output_dir_path: Path, report: dict, output_file_name: str = 'classification_report.json'):
+    """
+    Saves the given report dictionary to a JSON file.
+
+    Args:
+        output_dir_path (Path): Path to the directory where report files are stored.
+        report (dict): The report containing all metrics and confusion matrix.
+        output_file_name (Optional[str]): Filename for the report, defaults to 'classification_report.json'.
+    """
+    output_path = output_dir_path / output_file_name
+    output_dir_path.mkdir(parents=True, exist_ok=True)
+
+    with open(output_path, 'w') as f:
+        json.dump(report, f, indent=4)
+
+def generate_report(actual_annotations: List[str], predicted_annotations: List[str], 
+                    classes: Tuple[str], total_time_seconds: float, peak_memory_mb: float) -> dict:
     """
     Generates and saves a report based on classification results.
 
     Args:
-        output_dir_path (Path): Path to the directory where report files are stored.
         actual_annotations (List[str]): List of actual annotation labels.
         predicted_annotations (List[str]): List of predicted annotation labels.
         classes (List[str]): List of all class labels.
         total_time_seconds (float): Time in seconds needed for the classification process.
         peak_memory_mb (float): Peak memory in MB during the classification process.
+
+    Returns:
+        dict: The report containing all metrics and confusion matrix.
     """
     c_matrix = confusion_matrix(actual_annotations, predicted_annotations, labels=classes)
     accuracy = accuracy_score(actual_annotations, predicted_annotations)
@@ -132,24 +152,18 @@ def generate_report(output_dir_path: Path, actual_annotations: List[str], predic
     weighted_avg_recall = recall_score(actual_annotations, predicted_annotations, average='weighted')
     weighted_avg_f1_score = f1_score(actual_annotations, predicted_annotations, average='weighted')
 
-    report = list()
-    matrix_str = '\n'.join(['\t'.join(map(str, row)) for row in c_matrix])
-    report.append(f'Matrix:\n{matrix_str}')
-    report.append(f'Accuracy: {accuracy}')
-    #report.append(f'Weighted Avg. Precision: {weighted_avg_precision}')
-    report.append(f'Weighted Avg. Recall: {weighted_avg_recall}')
-    report.append(f'Weighted Avg. F1 Score: {weighted_avg_f1_score}')
-    report.append(f'Classification Time (s): {total_time_seconds}')
-    report.append(f'Peak Memory (mb): {peak_memory_mb}')
+    report = {
+        'confusion_matrix': c_matrix.tolist(),
+        'accuracy': accuracy,
+        'weighted_avg_recall': weighted_avg_recall,
+        'weighted_avg_f1_score': weighted_avg_f1_score,
+        'classification_time_seconds': total_time_seconds,
+        'peak_memory_mb': peak_memory_mb
+    }
 
-    output_file_name = 'classification_report.txt'
-    output_path = output_dir_path / output_file_name
+    return report
 
-    with open(output_path, 'w') as f:
-        for line in report:
-            f.write(f'{line}\n')
-
-def process_files(window_size: int, window_overlap: int, model_file_path: Path, classes: Tuple[str], relevant_columns: Optional[List[str]], input_dir_path: Path, output_dir_path) -> None:
+def process_files(window_size: int, window_overlap: int, model_file_path: Path, classes: Tuple[str], input_dir_path: Path, output_dir_path) -> None:
     """
     Processes all CSV files in the input directory by segmenting them into windows,
     classifying the windows, and generating a classficiation result in the output directory.
@@ -159,7 +173,6 @@ def process_files(window_size: int, window_overlap: int, model_file_path: Path, 
         overlap_size (int): The number of overlapping rows between consecutive windows.
         model_file_path (Path): Path to the .eim model file.
         classes (Tuple[str]): Classes meant to be classified by the selected model.
-        relevant_columns (Optional[List[str]]): The list of columns expected by the model, all other are dropped.
         input_dir (Path): Directory containing input CSV files.
         output_dir (Path): Directory to write output files.
     
@@ -178,7 +191,7 @@ def process_files(window_size: int, window_overlap: int, model_file_path: Path, 
     for file in files:
         print(f'Processing file {file}...')
         episode_df = read_csv_to_pandas_dataframe(file)
-        ep_actual_annotations, ep_predicted_annotations = classify_window_by_window(episode_df, window_size, window_overlap, loaded_model, relevant_columns)
+        ep_actual_annotations, ep_predicted_annotations = classify_window_by_window(episode_df, window_size, window_overlap, loaded_model)
         
         actual_annotations.extend(ep_actual_annotations)
         predicted_annotations.extend(ep_predicted_annotations)
@@ -192,8 +205,8 @@ def process_files(window_size: int, window_overlap: int, model_file_path: Path, 
 
     close_loaded_model(loaded_model)
 
-    generate_report(output_dir_path, actual_annotations, predicted_annotations, classes, total_time_seconds, peak_memory_mb)
-
+    report = generate_report(actual_annotations, predicted_annotations, classes, total_time_seconds, peak_memory_mb)
+    save_report_to_json_file(output_dir_path, report)
 
 if __name__ == '__main__':
     # Parameters to be set
@@ -204,13 +217,10 @@ if __name__ == '__main__':
     # Classes
     classes = ('getting up', 'leaving home', 'entering home', 'preparing for bed', 'airing', 'sleeping', 'working', 'working out', 'relaxing', 'preparing breakfast', 'eating breakfast', 'preparing dinner', 'eating dinner', 'preparing supper', 'eating supper', 'preparing a drink', 'preparing a meal', 'eating a meal', 'other')
 
-    # Columns
-    relevant_columns = ['kitchen humidity [%]', 'kitchen luminosity [Lux]', 'kitchen magnitude accelerometer [m/s²]', 'kitchen temperature [°C]', 'entrance humidity [%]', 'entrance luminosity [Lux]', 'entrance magnitude accelerometer [m/s²]', 'entrance temperature [°C]', 'living room humidity [%]', 'living room luminosity [Lux]', 'living room magnitude accelerometer [m/s²]', 'living room temperature [°C]', 'living room CO2 [ppm]', 'living room max sound pressure [dB]', 'hour']
-
     # Paths
     input_dir_path = get_input_path()
     output_dir_path = get_output_path()
     output_dir_path.mkdir(parents=True, exist_ok=True)
     model_file_path = input_dir_path / model_file_name
 
-    process_files(window_size, window_overlap, model_file_path, classes, relevant_columns, input_dir_path, output_dir_path)
+    process_files(window_size, window_overlap, model_file_path, classes, input_dir_path, output_dir_path)
