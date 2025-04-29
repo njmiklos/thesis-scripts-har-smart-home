4bb62681799a8a6208f0376dbc958ca74dccbcf7
Natalia Miklós <natalia.j.miklos@gmail.com>
2025-04-29 08:21:23 +0200

    Tracing time and memory for single classification rather than the whole process

--- src/test_eim.py
+++ src/test_eim.py
@@ -80,7 +80,8 @@ def validate_input(total_rows: int, window_size: int, overlap_size: int) -> bool
 
     return True
 
-def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: int, loaded_model: ImpulseRunner) -> Tuple[Optional[List[str]], Optional[List[str]]]:
+def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: int, 
+    loaded_model: ImpulseRunner) -> Tuple[Optional[List[str]], Optional[List[str]], Optional[List[float]], Optional[List[float]]]:
     """
     Segments a DataFrame into overlapping windows and classifies each.
 
@@ -91,8 +92,10 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
         loaded_model (ImpulseRunner): Loaded .eim model.
 
     Returns:
-        Tuple[Optional[List[str]], Optional[List[str]]]: If input is valid, a Tuple with actual and predicted annotations 
-            for all windows. Empty otherwise.
+        Tuple[Optional[List[str]], Optional[List[str]], Optional[List[float]], Optional[List[float]]]: 
+            If input is valid, a Tuple with actual and predicted annotations for all windows,
+            longest time and peak memory a single classification took in the episode. 
+            Empty otherwise.
     """
     total_rows = len(df)
 
@@ -101,6 +104,9 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
     if input_valid:
         predicted_annotations = list()
         actual_annotations = list()
+
+        time_longest_across_episode = 0
+        memory_peak_across_episode = 0
 
         start_position = 0
         segment_count = 1
@@ -112,17 +118,34 @@ def classify_window_by_window(df: pd.DataFrame, window_size: int, overlap_size: 
             last_annotation = window['annotation'].iloc[-1]
             actual_annotations.append(last_annotation)
 
+            start_time = time.perf_counter()
+
+            tracemalloc.start()
+
             formatted_window = format_window_for_classification(window)
             classification_result = classify_window(loaded_model, formatted_window)
             prediction_class, _ = get_top_prediction(classification_result)
             predicted_annotations.append(prediction_class)
             
+            end_time = time.perf_counter()
+            time_longest_within_window_s = end_time - start_time
+            time_longest_within_window_ms = time_longest_within_window_s * 1000
+
+            _, memory_peak_within_window_b = tracemalloc.get_traced_memory()
+            tracemalloc.stop()
+            memory_peak_within_window_kb = memory_peak_within_window_b / 1024
+
+            if time_longest_within_window_ms > time_longest_across_episode:
+                time_longest_across_episode = time_longest_within_window_ms
+            if memory_peak_within_window_kb > memory_peak_across_episode:
+                memory_peak_across_episode = memory_peak_within_window_kb
+
             start_position += (window_size - overlap_size)
             segment_count += 1
 
-        return actual_annotations, predicted_annotations
+        return actual_annotations, predicted_annotations, time_longest_across_episode, memory_peak_across_episode
 
-    return None, None
+    return None, None, None, None
 
 def save_report_to_json_file(output_dir_path: Path, report: dict, output_file_name: str = 'classification_report.json'):
     """
@@ -140,15 +163,15 @@ def save_report_to_json_file(output_dir_path: Path, report: dict, output_file_na
         json.dump(report, f, indent=4)
 
 def generate_report(actual_annotations: List[str], predicted_annotations: List[str], 
-                    total_time_seconds: float, peak_memory_mb: float) -> dict:
+                    total_time_ms: float, peak_memory_kb: float) -> dict:
     """
     Generates and saves a report based on classification results.
 
     Args:
         actual_annotations (List[str]): List of actual annotation labels.
         predicted_annotations (List[str]): List of predicted annotation labels.
-        total_time_seconds (float): Time in seconds needed for the classification process.
-        peak_memory_mb (float): Peak memory in MB during the classification process.
+        total_time_ms (float): Time in ms needed for the classification process.
+        peak_memory_kb (float): Peak memory in KB during the classification process.
 
     Returns:
         dict: The report containing all metrics and confusion matrix.
@@ -166,8 +189,8 @@ def generate_report(actual_annotations: List[str], predicted_annotations: List[s
         'accuracy': accuracy,
         'weighted_avg_recall': weighted_avg_recall,
         'weighted_avg_f1_score': weighted_avg_f1_score,
-        'classification_time_seconds': total_time_seconds,
-        'peak_memory_mb': peak_memory_mb
+        'classification_time_ms': total_time_ms,
+        'peak_memory_kb': peak_memory_kb
     }
 
     return report
@@ -191,31 +214,30 @@ def process_files(window_size: int, window_overlap: int, model_file_path: Path, 
     predicted_annotations = list()
     actual_annotations = list()
     
-    loaded_model = load_model(model_file_path)
+    time_longest_across_episodes = 0
+    memory_peak_across_episodes = 0
 
-    start_time = time.perf_counter()
-    tracemalloc.start()
+    loaded_model = load_model(model_file_path)
 
     for file in files:
         print(f'Processing file {file}...')
         episode_df = read_csv_to_pandas_dataframe(file)
-        ep_actual_annotations, ep_predicted_annotations = classify_window_by_window(episode_df, window_size, window_overlap, loaded_model)
+        ep_actual_annotations, ep_predicted_annotations, ep_time_longest, ep_memory_peak = classify_window_by_window(episode_df, window_size, window_overlap, loaded_model)
         
         if ep_actual_annotations and ep_predicted_annotations:
             actual_annotations.extend(ep_actual_annotations)
             predicted_annotations.extend(ep_predicted_annotations)
 
-    end_time = time.perf_counter()
-    total_time_seconds = end_time - start_time
-
-    _, peak_memory_bytes = tracemalloc.get_traced_memory()
-    tracemalloc.stop()
-    peak_memory_mb = peak_memory_bytes / (1024 * 1024)
+            if ep_time_longest > time_longest_across_episodes:
+                time_longest_across_episodes = ep_time_longest
+            if ep_memory_peak > memory_peak_across_episodes:
+                memory_peak_across_episodes = ep_memory_peak
 
     close_loaded_model(loaded_model)
 
-    report = generate_report(actual_annotations, predicted_annotations, total_time_seconds, peak_memory_mb)
+    report = generate_report(actual_annotations, predicted_annotations, time_longest_across_episodes, memory_peak_across_episodes)
     save_report_to_json_file(output_dir_path, report)
+
 
 if __name__ == '__main__':
     # Parameters to be set
