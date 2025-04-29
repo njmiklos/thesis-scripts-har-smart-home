6455b9392ea9b16e44fc48725c3a1f9f10bc3fe4
Natalia Miklós <natalia.j.miklos@gmail.com>
2025-04-29 13:16:39 +0200

    Combined similar code

--- src/visualize_ei_reports.py
+++ src/visualize_ei_reports.py
@@ -1,10 +1,16 @@
+"""
+This file is used to visualize:
+- a training process based on a formatted log report downloaded from Edge Impulse.
+- a confusion matrix based on a JSON classification report downloaded from Edge Impulse.
+"""
 import json
 import numpy as np
 
 from pathlib import Path
 
 from get_env import get_input_path, get_output_path
-from visualize_data import generate_confusion_matrix
+from handle_csv import read_csv_to_pandas_dataframe
+from visualize_data import generate_confusion_matrix, generate_comparative_timeseries_plot
 
 
 def load_json_file(file_path: Path) -> dict:
@@ -19,6 +25,32 @@ def load_json_file(file_path: Path) -> dict:
     """
     with open(file_path, 'r') as file:    
         return json.load(file)
+
+def visualize_training_process(training_process_file_path: Path, output_dir_path: Path) -> None:
+    """
+    Visualizes the changes in training and validation accuracy and loss over the epochs.
+
+    Args:
+        training_process_file_path (Path): Path to the CSV file containing training metrics, extracted
+            from the training log. The format must be like in the following example:
+                Epoch,Loss,Accuracy,Validation Loss,Validation Accuracy
+                1,0.9531,0.8597,0.5308,0.8722
+                2,0.4738,0.8813,0.3968,0.8923
+                3,0.3688,0.8963,0.3293,0.9016
+        output_dir_path (Path): Directory where the visualizations will be saved.
+
+    Returns:
+        None
+    """
+    training_df = read_csv_to_pandas_dataframe(training_process_file_path)
+
+    generate_comparative_timeseries_plot(training_df['Epoch'], training_df['Accuracy'], training_df['Validation Accuracy'], 
+                                         'Training Accuracy', 'Validation Accuracy', 'Model Training - Accuracy Over Time', 'Epoch',
+                                         'Accuracy', output_dir_path)
+
+    generate_comparative_timeseries_plot(training_df['Epoch'], training_df['Loss'], training_df['Validation Loss'], 
+                                         'Training Loss', 'Validation Loss', 'Model Training - Loss Over Time', 'Epoch',
+                                         'Loss', output_dir_path)
     
 def convert_matrix_values_to_percentages(matrix: np.ndarray) -> np.ndarray:
     """
@@ -61,15 +93,17 @@ def visualize_confusion_matrix(data: dict[str, dict], model_version: str, output
 
 if __name__ == '__main__':
     # Paths
-    input_file_name = 'model-routines-testing-results.json'
-    input_file_path = get_input_path() / input_file_name
-    output_dir_path = get_output_path()
+    classification_report_file_name = 'model-routines-testing-results.json'
+    training_process_file_name = 'epochs.csv'
 
+    classification_report_path = get_input_path() / classification_report_file_name
+    training_process_file_path = get_input_path() / training_process_file_name
+    output_dir_path = get_output_path()
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    report = load_json_file(input_file_path)
+    report = load_json_file(classification_report_path)
 
-    # Adjust set and version model if necessary
     #visualize_confusion_matrix(report['validation'], 'float32', output_dir_path)
     visualize_confusion_matrix(report['test'], 'float32', output_dir_path)
 
+    visualize_training_process(training_process_file_path, output_dir_path)
