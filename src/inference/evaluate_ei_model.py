58e470f188b5e0993adc48bba43bd5498a4164ca
Natalia Miklós <natalia.j.miklos@gmail.com>
2025-05-02 14:09:27 +0200

    Moved to correct directory

--- src/inference/evaluate_ei_model.py
+++ src/inference/evaluate_ei_model.py
@@ -20,7 +20,7 @@ from utils.get_env import get_path_from_env
 from utils.handle_csv import read_csv_to_pandas_dataframe, get_all_csv_files_in_directory
 from inference.classify_with_ei_model import load_model, close_loaded_model, classify_window, get_top_prediction
 from inference.evaluation_utils import ClassificationResults, TimeMemoryTracer
-from inference.visualize_ei_report import convert_matrix_values_to_percentages
+from data_analysis.visualize_ei_report import convert_matrix_values_to_percentages
 from data_analysis.visualize_data import generate_confusion_matrix
 
 
