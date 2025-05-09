517b13a3ccbde25c632b35817825a1461123f1e2
Natalia Miklós <natalia.j.miklos@gmail.com>
2025-05-09 18:39:17 +0200

    Finished draft

--- src/inference/process_windows_with_fm.py
+++ src/inference/process_windows_with_fm.py
@@ -3,12 +3,14 @@ This code grabs formatted windows from a JSON file, sends them to an API of a Fo
 tracks the process, and saves results and metrics into a JSON file.
 """
 from pathlib import Path
-from typing import List, Optional
+from typing import List
 
 from utils.get_env import get_path_from_env
+from utils.file_handler import save_to_json_file, load_json_file
 from data_processing.compress_for_fm import Window
-from inference.evaluation_utils import ClassificationResults
-from inference.evaluate_ei_model import save_to_json_file
+from inference.query_fm_api import send_chat_request
+from inference.evaluate.utils import TimeMemoryTracer
+
 
 class ExtendedWindow(Window):
     """
@@ -50,61 +52,6 @@ class ExtendedWindow(Window):
         window_dict = super().to_dictionary()
         window_dict['tokens'] = self.tokens
         return window_dict
-
-class ClassificationResultsFM(ClassificationResults):
-    """
-    Extends 'ClassificationResults' class with 'total_prompt_tokens'.
-
-    Args:
-        actual_annotations (Optional[List[str]]): Combined list of all actual annotations.
-        predicted_annotations (Optional[List[str]]): Combined list of all predicted annotations.
-        max_classification_time_ms (float): The highest classification time (ms) observed.
-        max_classification_memory_kb (float): The highest memory usage (kB) observed.
-        total_prompt_tokens (int): The total number of tokens needed for the classification.
-    """
-    def __init__(self, actual_annotations: Optional[List[str]] = None, predicted_annotations: Optional[List[str]] = None,
-        max_classification_time_ms: float = 0.0, max_classification_memory_kb: float = 0.0, total_prompt_tokens: int = 0) -> None:
-        if total_prompt_tokens < 0:
-            raise ValueError(f'Number of tokens must be larger than 0, got {total_prompt_tokens}')
-        
-        super().__init__(actual_annotations, predicted_annotations, max_classification_time_ms, max_classification_memory_kb)
-        self.total_prompt_tokens: int = total_prompt_tokens
-
-    def update(self, other: 'ClassificationResultsFM') -> None:
-        """
-        Merges another ClassificationResults into this one, in place.
-        """
-        super().update(other)
-        self.total_prompt_tokens += other.total_prompt_tokens
-
-    def generate_report(self, total_classification_time_secs: float) -> dict:
-        """
-        Generates a performance report from the aggregated classification results.
-
-        Args:
-            total_classification_time_secs (float): Total classification time in seconds.
-
-        Returns:
-            dict: A report containing:
-                - 'classes' (List[str]): A sorted list of unique true and false annotations.
-                - 'confusion_matrix' (List[List[int]]): Confusion matrix between true and predicted labels.
-                - 'total_no_predictions' (int): Total number of predictions made (i.e., number of windows).
-                - 'accuracy' (float): Overall classification accuracy.
-                - 'weighted_avg_precision' (float): Weighted average precision.
-                - 'weighted_avg_recall' (float): Weighted average recall.
-                - 'weighted_avg_f1_score' (float): Weighted average F1 score.
-                - 'classification_time_ms' (float): The worst-case classification time (ms).
-                - 'peak_memory_kb' (float): The worst-case memory usage (kB),
-                - 'total_classification_time_secs' (float): Total classification time in seconds.
-                - 'total_prompt_tokens' (int): Total number of tokens needed for the classification.
-        """
-        report = super().generate_report(total_classification_time_secs)
-        report['total_prompt_tokens'] = self.total_prompt_tokens
-        return report
-    
-def read_from_json_file(input_dir_path: Path) -> dict:
-    with open(input_dir_path, 'r') as file:
-        return json.load(file)
 
 def validate_window_as_dict(dictionary: dict) -> None:
     """
@@ -166,8 +113,58 @@ def get_prompt(stage: int, input_dir_path: Path) -> str:
 
     with open(input_dir_path / filename) as f:
         return f.read()
+
+def convert_window_list_to_dict_list(windows: List['ExtendedWindow']) -> List[dict]:
+    """
+    Convert the list of Window objects to a list of dictionaries.
+
+    Args:
+        windows (List['Window']): A list of Window objects.
     
-def process_windows(model_name: str, stage: int, input_dir_path: Path, output_dir_path: Path) -> None:
+    Returns:
+        List[dict]: A list of dictionaries representing Window objects.
+    """
+    return [window.to_dictionary() for window in windows]
+
+def save_windows(output_dir_path: Path, windows: List['Window'], windows_per_file: int, stage: int) -> None:
+    """
+    Saves Windows to JSON. If windows_per_file > 0, writes that many
+    windows per file. Otherwise, writes all windows to a single file.
+
+    Args:
+        output_dir_path (Path): Directory where the final JSON data will be saved.
+        windows (List['Window']): A list of Window objects.
+        windows_per_file (int): Number of windows to be saved per file. If 0 is given,
+            all windows are saved to the same file.
+        stage (int): Stage of classification with an FM, either 1 or 2. It definies the prompt content.
+            Stage 1 summarizes and stage 2 classfies. (Prompt texts are intentionally left out. 
+            Please refer to the finished thesis.)
+
+    Returns:
+        None
+    """
+    if windows_per_file < 0:
+        raise ValueError(f'Window number must be ≥ 0, got {windows_per_file}.')
+    
+    total_windows = len(windows)
+    file_counter = 0
+
+    if windows_per_file > 0:
+        for start_pos in range(0, total_windows, windows_per_file):
+            file_counter += 1
+            end_pos = start_pos + windows_per_file
+            chunk = windows[start_pos : end_pos]
+            chunk_dicts = convert_window_list_to_dict_list(chunk)
+            filename = f'compressed_windows_{file_counter}.json'
+            save_to_json_file(output_dir_path, chunk_dicts, filename)
+    else:
+        file_counter = 1
+        dicts = convert_window_list_to_dict_list(windows)
+        save_to_json_file(output_dir_path, dicts, f'windows_stage_{stage}.json')
+    
+    print(f'Saved {total_windows} window(s) to {file_counter} file(s).')
+
+def process_windows(model_name: str, stage: int, input_dir_path: Path, output_dir_path: Path, windows_per_file: int) -> None:
     """
     Processes windows saved in a JSON file, and writes a combined result to JSON.
 
@@ -177,44 +174,50 @@ def process_windows(model_name: str, stage: int, input_dir_path: Path, output_di
             Please refer to the finished thesis.)
         input_dir_path (Path): Directory containing the input to process.
         output_dir_path (Path): Directory where the final JSON data will be saved.
+        windows_per_file (int): Number of windows to be saved per file. If 0 is given,
+            all windows are saved to the same file.
     
     Returns:
         None
     """
-    windows_dict = read_from_json_file(input_dir_path)
+    windows_dict = load_json_file(input_dir_path)
     windows = convert_dict_list_to_window_list(windows_dict)
-
-    results = list()
 
     for counter, window in enumerate(windows, start=1):
         print(f'Working on window {counter}/{len(windows)}...')
+        resource_tracker = TimeMemoryTracer()
 
         validate_window(window)
 
         if stage == 1:
             prompt = get_prompt(1, input_dir_path)
-            result = summarize_data_with_fm(model_name, prompt, window)
         else:
             prompt = get_prompt(2, input_dir_path)
-            result = classify_summary_with_fm(model_name, prompt, window)
-        
-        results.append(result)
+        response = send_chat_request(model=model_name, prompt=prompt, user_message=window.data)
+        response_json = response.json()
 
-        check_limits(results)
+        _, max_memory_kb = resource_tracker.stop()
+
+        latency = response_json.elapsed
+        system_text = response_json['choices'][0]['message']['content']
+        total_tokens = response_json['usage']['total_tokens']
+
+        window_update = ExtendedWindow(window.true_annotation, system_text, latency, max_memory_kb, total_tokens)
+        window.update(window_update)
 
     print(f'Done.')
 
-    results_dict = convert_results_list_to_dict_list(results)
-    save_to_json_file(output_dir_path, results_dict, f'windows_stage_{stage}.json')
+    save_windows(output_dir_path, windows, windows_per_file, stage)
 
 
 if __name__ == '__main__':
     stage = 1
     model_name = 'llama-3.3-70b-instruct'
+    windows_per_file = 90    # If 0 is given, all windows are saved to the same file.
 
     input_dir_path = get_path_from_env('INPUTS_PATH')
     output_dir_path = get_path_from_env('OUTPUTS_PATH')
 
     output_dir_path.mkdir(parents=True, exist_ok=True)
 
-    process_windows(model_name, stage, input_dir_path, output_dir_path)
+    process_windows(model_name, stage, input_dir_path, output_dir_path, windows_per_file)
